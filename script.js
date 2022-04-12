0352f93675ab7d307c1aeeaab94ed89cd3d1d92b
Nico Addai <nico@gradienthealth.io>
2022-04-12 09:11:47 -0400

    Start of new DOM project

---
+++ script.js
@@ -0,0 +1,10 @@
+//set inital values 
+
+let pomodoro = 0; 
+
+//select value and buttons 
+
+const timeEl = document.querySelector("#time"); 
+console.log(timeEl);
+const buttonsEl = document.queurySelectorAll("buttons");           
+console.log(buttonsEl); 
